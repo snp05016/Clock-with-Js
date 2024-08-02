cde591bd32cd14c30c09e365eff4af55d3956776
snp050106 <snp050106@gmail.com>
2024-08-02 15:14:18 +0400

    centered the clock and chagned the gradient

--- clock.js
+++ clock.js
@@ -4,7 +4,7 @@ function updateclock(){
     const ampm = today.getHours() >= 12? 'PM' : 'AM';
     let minutes = today.getMinutes().toString().padStart(2, 0);
     let seconds = today.getSeconds().toString().padStart(2, 0);
-    const timestring = `${hours}:${minutes}:${seconds} ${ampm}`;
+    const timestring = ` ${hours}:${minutes}:${seconds}  ${ampm}`;
     document.getElementById('clock').textContent = timestring;
 }
 updateclock();
